6fa73f919b9698b689b27d3d31b9641380d1f399
nazir-ahmadi <n.ahmadi@tekschool.us>
2022-11-10 18:17:08 -0800

    Branch Commit

---
+++ src/Test/testClass.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class testClass {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("This branch is for practice pull and push to remote repo");
+	}
+
+}
